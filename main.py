39c7a3f7b0d0b6814ca114d7083e3f478fb0c457
steven0129 <h3584935@gmail.com>
2018-10-17 20:13:43 +0800

    Pair generation

--- main.py
+++ main.py
@@ -28,20 +28,29 @@ def pair_gen(**kwargs):
     for k_, v_ in kwargs.items():
         setattr(options, k_, v_)
 
-    open('pair.csv', 'a').write('summary,text\n')
+    PAIR = open('pair.csv', 'a')
+    ERROR = open('error.txt', 'a')
+    data = open('sky_dragon_traditional.txt').read().replace('\n', '<newline>')
+    array = data.split('<newline>')
+
+    PAIR.write('summary,text\n')
     for i in tqdm(range(1,51)):
         with open(f'./csv/天龍八部_大意{str(i).zfill(2)}.csv') as f:
-            reader = csv.DictReader(f, delimiter=',')
-            data = open('sky_dragon_traditional.txt').read().replace('\n', '<newline>')
-            array = data.split('<newline>')
-            for row in tqdm(reader):
-                begin = row['此段落的最初2-5句話']
-                end = row['此段落的最末2-5句話']
-                summary = row['段落大意']
-                begin = process.extractOne(begin, tqdm(array))[0]
-                end = process.extractOne(end, tqdm(array))[0]
-                text = ''.join(list(data)[data.find(begin) : data.find(end) + len(end)])
-                open('pair.csv', 'a').write(f'{summary},{text}\n')
+            reader = csv.reader(f, delimiter=',')
+            
+            for idx, row in enumerate(tqdm(reader)):
+                if idx != 0:
+                    begin = row[0]
+                    end = row[1]
+                    summary = row[2]
+                
+                    begin = process.extractOne(begin, tqdm(array))[0]
+                    end = process.extractOne(end, tqdm(array))[0]
+                    text = ''.join(list(data)[data.find(begin) : data.find(end) + len(end)])
+                    if text == '':
+                        ERROR.write(f'FILE{i} 編號{idx} 空字串: ' + ','.join(row))
+                    
+                    PAIR.write(f'{summary},{text}\n')
 
 if __name__ == '__main__':
     fire.Fire()
